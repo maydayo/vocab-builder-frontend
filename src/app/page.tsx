199b327c748056af4939b311fdc67778c16e0dfc
maydayo <wanwisa.silanan@gmail.com>
2024-03-16 23:18:46 +0700

    feat: move to components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { ParagraphGenerator } from "./_components/ParagraphGeneratorBox";
-import { VocabularyAddBox } from "./_components/VocabularyAddBox";
+import { ParagraphGenerator } from "../components/ParagraphGeneratorBox";
+import { VocabularyAddBox } from "../components/VocabularyAddBox";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-5 p-24">
       <VocabularyAddBox />
