f2a87579cb98ba3a0bc361a7eacf0d5e43bd23d0
maydayo <wanwisa.silanan@gmail.com>
2024-06-15 01:24:16 +0700

    add dropdown

--- src/app/(protect)/folders/[folderId]/list/_components/LearningStatusButton.tsx
+++ src/app/(protect)/folders/[folderId]/list/_components/LearningStatusButton.tsx
@@ -1,5 +1,12 @@
-import { Vocabulary } from "@/types/vocabulary.type";
+import { LearningStatus, Vocabulary } from "@/types/vocabulary.type";
 import { useUpdateLearningStatus } from "./useUpdateLearningStatus";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import {
+  AcademicCapIcon,
+  BookOpenIcon,
+  ChevronDownIcon,
+  PauseCircleIcon,
+} from "@heroicons/react/16/solid";
 
 type LearningStatusButtonProps = {
   vocabulary: Vocabulary;
@@ -11,10 +18,12 @@ export function LearningStatusButton(props: LearningStatusButtonProps) {
   const { updateLearningStatusAsync, isPending: isUpdateStatusPending } =
     useUpdateLearningStatus();
 
-  async function onClickToggleStatus(vocabulary: Vocabulary) {
-    const learningStatus =
-      vocabulary.learningStatus === "learning" ? "learned" : "learning";
-    updateLearningStatusAsync({ vocabularyId: vocabulary.id, learningStatus })
+  async function onClickToggleStatus(
+    vocabularyId: string,
+    newStatus: LearningStatus
+  ) {
+    const learningStatus = newStatus;
+    updateLearningStatusAsync({ vocabularyId, learningStatus })
       .then(() => {
         onUpdateStatusSuccess();
       })
@@ -24,15 +33,61 @@ export function LearningStatusButton(props: LearningStatusButtonProps) {
   }
 
   return (
-    <button
-      onClick={() => onClickToggleStatus(vocabulary)}
-      disabled={isUpdateStatusPending}
-    >
-      {isUpdateStatusPending ? (
-        <span className="loading-spinner" />
-      ) : (
-        <>{vocabulary.learningStatus}</>
-      )}
-    </button>
+    <Menu>
+      <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+        {isUpdateStatusPending ? (
+          <span className="loading-spinner" />
+        ) : (
+          <>{vocabulary.learningStatus}</>
+        )}
+        <ChevronDownIcon className="size-4 fill-white/60" />
+      </MenuButton>
+
+      <MenuItems
+        anchor="bottom end"
+        className="w-52 origin-top-right rounded-xl border border-white/5 bg-gray-800 p-1 text-sm/6 text-white [--anchor-gap:var(--spacing-1)] focus:outline-none"
+      >
+        <MenuItem>
+          <button
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+            onClick={() => onClickToggleStatus(vocabulary.id, "idle")}
+          >
+            <PauseCircleIcon className="size-4 fill-white/30" />
+            Idle
+          </button>
+        </MenuItem>
+        <MenuItem>
+          <button
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+            onClick={() => onClickToggleStatus(vocabulary.id, "learning")}
+          >
+            <BookOpenIcon className="size-4 fill-white/30" />
+            Learning
+          </button>
+        </MenuItem>
+        <div className="my-1 h-px bg-white/5" />
+        <MenuItem>
+          <button
+            className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+            onClick={() => onClickToggleStatus(vocabulary.id, "learned")}
+          >
+            <AcademicCapIcon className="size-4 fill-white/30" />
+            Learned
+          </button>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+
+    // <button
+    //   onClick={() => onClickToggleStatus(vocabulary)}
+    //   disabled={isUpdateStatusPending}
+    // >
+    //   {isUpdateStatusPending ? (
+    //     <span className="loading-spinner" />
+    //   ) : (
+    //     <>{vocabulary.learningStatus}</>
+    //   )}
+
+    // </button>
   );
 }
